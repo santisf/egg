01298d776806d31617ca7559848e5be01144f043
santisf <68931468+santisf@users.noreply.github.com>
2023-08-22 20:54:15 -0300

    Add files via upload

---
+++ ejercicio2ERelaciones/src/Main.java
@@ -0,0 +1,15 @@
+import Entidades.Cine;
+import Servicio.CineServicio;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        CineServicio cS= new CineServicio();
+        Cine cine;
+        cine= cS.mostrarCine();
+        System.out.println(cine.getSala()[3][4].toString());
+
+
+    }
+}
